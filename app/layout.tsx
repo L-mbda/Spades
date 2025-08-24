45d89c86daa02c2952810c9419f166fefa720e30
Lambda <61808223+L-mbda@users.noreply.github.com>
2025-08-23 21:07:03 -0700

    Commit Potential Updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,21 @@
 import type { Metadata } from "next";
-import { Libre_Franklin } from "next/font/google";
+import { Libre_Franklin, Mulish, DM_Sans } from "next/font/google";
 import "./globals.css";
-import '@mantine/core/styles.css';
+import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 
 const libreFranklin = Libre_Franklin({
   variable: "--font-libre-franklin",
+  subsets: ["latin"],
+});
+
+const mulish = Mulish({
+  variable: "--font-mulish",
+  subsets: ["latin"],
+});
+
+const dmsans = DM_Sans({
+  variable: "--font-dmsans",
   subsets: ["latin"],
 });
 
@@ -22,11 +32,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${libreFranklin.variable} antialiased`}
+        className={`${libreFranklin.variable} ${mulish.variable} ${dmsans.variable} antialiased`}
       >
-        <MantineProvider>
-          {children}
-        </MantineProvider>
+        <MantineProvider>{children}</MantineProvider>
       </body>
     </html>
   );
